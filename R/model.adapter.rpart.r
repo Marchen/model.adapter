68979127f81447333d95c4a0f248f9cadafd6485
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:48:10 +0900

    Added model.adapter class for rpart. rpart用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.rpart.r
@@ -0,0 +1,14 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for rpart
+#'
+#'	This reference class contains methods for \code{\link[rpart]{rpart}} in 
+#'	\emph{rpart} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	rpart関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.rpart <- setRefClass(
+	"model.adapter.rpart", contains = "model.adapter"
+)
+
+
