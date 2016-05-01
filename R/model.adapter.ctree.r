d81defacedaf2845a4ce0333688c00b28a5e18f8
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:29:13 +0900

    Added model.adapter class for ctree. ctree用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.ctree.r
@@ -0,0 +1,14 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for ctree
+#'
+#'	This reference class contains methods for \code{\link[party]{ctree}} in 
+#'	\emph{party} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	ctree関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.ctree <- setRefClass(
+	"model.adapter.ctree", contains = "model.adapter"
+)
+
+
