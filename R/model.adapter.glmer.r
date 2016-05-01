4e100ec8cbfd906196503eddb516ce4dad52f1f5
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:07:53 +0900

    Added model.adapter class for glmer. glmer用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.glmer.r
@@ -0,0 +1,12 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for glmer
+#'
+#'	This reference class contains methods for \code{\link[lme4]{glmer}} in 
+#'	\emph{lme4} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	glmer関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.glmer <- setRefClass(
+	"model.adapter.glmer", contains = "model.adapter"
+)
