b6476844afee4dca354355585e24ce0c203a8bbb
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:46:11 +0900

    Added model.adapter class for tree. tree用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.tree.r
@@ -0,0 +1,14 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for tree
+#'
+#'	This reference class contains methods for \code{\link[tree]{tree}} in 
+#'	\emph{tree} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	tree関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.tree <- setRefClass(
+	"model.adapter.tree", contains = "model.adapter"
+)
+
+
