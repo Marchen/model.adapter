e030ab55567a20a9cabb0fcf3fccd3c13fd713fa
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:24:57 +0900

    Added model.adapter class for svm. svm用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.svm.r
@@ -0,0 +1,13 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for svm
+#'
+#'	This reference class contains methods for \code{\link[e1071]{svm}} in 
+#'	\emph{e1071} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	svm関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.svm <- setRefClass(
+	"model.adapter.svm", contains = "model.adapter"
+)
+
