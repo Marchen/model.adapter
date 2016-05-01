ddf5f3eb6ed22a69b06e9dd336aef59ac85f9b93
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:20:56 +0900

    Added model.adapter class for lme. lme用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.lme.r
@@ -0,0 +1,12 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for lme
+#'
+#'	This reference class contains methods for \code{\link[nlme]{lme}} in 
+#'	\emph{nlme} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	lme関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.lme <- setRefClass(
+	"model.adapter.lme", contains = "model.adapter"
+)
