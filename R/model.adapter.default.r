f652daae632efb7f753d81a28fa5dfca02230fda
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 17:42:15 +0900

    Add model.adapter() function that initialize model.adapter object. model.adapter()関数を追加。

---
+++ R/model.adapter.default.r
@@ -0,0 +1,42 @@
+﻿#-------------------------------------------------------------------------------
+#'	Make a model.adapter object.
+#'
+#'	This function makes an object of a derived class of \emph{model.adapter} 
+#'	class that abstract differences in specifications of supported modeling 
+#'	functions.
+#'
+#'	@param x
+#'		an object of supported models or a call for a model function.
+#'	@return
+#'		an object of derived class of \code{\link{model.adapter-class}}.
+#'	@section For developpers
+#'		If x is an object containing result of statistical models, inheritance
+#'		of model.adapter is determined by usual S3 polymorphism. On the other
+#'		hand, if x is a call for a modeling function, inheritance of the class
+#'		is determined by function name in the function call. In such case, this
+#'		function call model.adapter.CLASS_NAME() to initialize object.
+#'		
+#'	@section Adding support for new function
+#'		To be continued...
+#-------------------------------------------------------------------------------
+#	モデルの違いを吸収するアダプタークラスのオブジェクトを作る関数。
+#
+#	Args:
+#		x: モデルオブジェクト、もしくはモデル関数の呼び出し。
+#
+#	Value:
+#		model.adapterクラスを継承したクラスのオブジェクト。
+#
+#-------------------------------------------------------------------------------
+model.adapter <- function(x) {
+	original.call <- substitute(x)
+	if (is.call(original.call)) {
+	   	fun.name <- as.character(original.call[1])
+	   	code <- sprintf(".model.adapter.%s$new()", fun.name)
+	   	object <- eval(parse(text = code))
+	   	return(object)
+	}
+	UseMethod("model.adapter")
+}
+
+
