d754008aa6f7723b9c32be6b020e7d45d3c2f897
Michio Oguro <michio_oguro@hotmail.com>
2016-05-02 01:23:53 +0900

    Added get.class.name() function which retrieve class name of a model object from function name. 関数名からオブジェクトのクラス名を取得するget.class.name()関数の追加。

---
+++ R/get.class.name.r
@@ -0,0 +1,32 @@
+#-------------------------------------------------------------------------------
+#	関数名からできあがるクラス名を取得する関数。
+#	対応してない関数は関数名がクラス名だと仮定して関数名を返す。
+#
+#	Args:
+#		function.name: 関数名
+#
+#	Value:
+#		その関数を実行したときに出来るオブジェクトのクラス名を表す文字列。
+#-------------------------------------------------------------------------------
+#' Find class name of a model object from function name
+#'
+#' @param function.name a character literal of function name
+#'
+#' @return a character literal of class name
+#'
+#' @examples
+#'	get.class.name("cforest")
+#-------------------------------------------------------------------------------
+get.class.name <- function(function.name){
+	class.name <- switch(
+		function.name,
+		cforest	= "RandomForest",
+		ctree	= "BinaryTree",
+		lmer	= "lmerMod",
+		glmer	= "glmerMod"
+	)
+	if (is.null(class.name)){
+		class.name <- function.name
+	}
+	return(class.name)
+}
