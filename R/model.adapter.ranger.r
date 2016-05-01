3ec2ad02c4c1984fd1e3f51717417b8b384f4c62
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:50:16 +0900

    Added model.adapter class for ranger. ranger用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.ranger.r
@@ -0,0 +1,14 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for ranger
+#'
+#'	This reference class contains methods for \code{\link[ranger]{ranger}} in 
+#'	\emph{ranger} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	ranger関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.ranger <- setRefClass(
+	"model.adapter.ranger", contains = "model.adapter"
+)
+
+
