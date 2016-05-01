6a898a7c1fec885ecba1acfe04a014e404539dee
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 17:54:02 +0900

    Added model.adapter class for gamm. gamm用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.gamm.r
@@ -0,0 +1,13 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for gamm
+#'
+#'	This reference class contains methods for \code{\link[mgcv]{gamm}} in 
+#'	\emph{mgcv} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	gamm関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+.model.adapter.gamm <- setRefClass(
+	"model.adapter.gamm", contains = "model.adapter"
+)
+
