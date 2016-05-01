91ae564c813d8ba1785cf941d1bb6954bce31c42
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:42:00 +0900

    Added model.adapter class for randomForest. randomForest用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.randomForest.r
@@ -0,0 +1,14 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for randomForest
+#'
+#'	This reference class contains methods for 
+#'	\code{\link[randomForest]{randomForest}} in \emph{randomForest} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	randomForest関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.randomForest <- setRefClass(
+	"model.adapter.randomForest", contains = "model.adapter"
+)
+
+
