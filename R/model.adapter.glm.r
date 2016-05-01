4b5a8e93dc24c2f2267062eb7b1a87b202a6ce77
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:03:37 +0900

    Added model.adapter class for glm. glm用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.glm.r
@@ -0,0 +1,13 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for glm
+#'
+#'	This reference class contains methods for \code{\link[stats]{glm}} in 
+#'	\emph{stats} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	glm関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.glm <- setRefClass(
+	"model.adapter.glm", contains = "model.adapter"
+)
+
