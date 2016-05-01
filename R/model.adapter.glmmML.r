097ad677a422963021f5365e50094027a0a24928
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:17:07 +0900

    Added model.adapter class for glmmML. glmmML用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.glmmML.r
@@ -0,0 +1,14 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for glmmML
+#'
+#'	This reference class contains methods for \code{\link[glmmML]{glmmML}} in 
+#'	\emph{glmmML} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	glmmML関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.glmmML <- setRefClass(
+	"model.adapter.glmmML", contains = "model.adapter"
+)
+
+
