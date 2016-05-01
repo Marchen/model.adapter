0f794c47505a06fc6e28a6fc93da9345ac3919d2
Michio Oguro <michio_oguro@hotmail.com>
2016-05-01 18:19:43 +0900

    Added model.adapter class for lm. lm用のmodel.adapterクラスの追加。

---
+++ R/model.adapter.lm.r
@@ -0,0 +1,15 @@
+﻿#-------------------------------------------------------------------------------
+#'	model.adapter class for lm
+#'
+#'	This reference class contains methods for \code{\link[stats]{lm}} in 
+#'	\emph{stats} package.
+#'	Following methods are overriden.
+#-------------------------------------------------------------------------------
+#	lm関数用のmodel.adapterオブジェクトのジェネレーター。
+#-------------------------------------------------------------------------------
+model.adapter.lm <- setRefClass(
+	"model.adapter.lm", contains = "model.adapter"
+)
+
+
+
