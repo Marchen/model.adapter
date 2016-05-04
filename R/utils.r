2d90249c6e9596ce8bb46905ab2945c1ef9da2b4
Michio Oguro <michio_oguro@hotmail.com>
2016-05-05 03:59:46 +0900

    Moved format.familiy() function to utils.r format.family()関数をutils.rに移動。

--- R/utils.r
+++ R/utils.r
@@ -99,3 +99,46 @@ is.formula <- function(x) {
 	return(is(x, "formula"))
 }
 
+
+#-------------------------------------------------------------------------------
+#'	(Internal) Format and check consistency of family.
+#'
+#'	@param family 
+#'		a family function, character of family name, symbol, or family object.
+#'	@param type
+#'		a character to specify the type of the value this function returns.
+#'		If "family", this function returns 'family' object. If "character" is
+#'		specified, this function returns a character string denoting the name
+#'		of the family.
+#'
+#'		This internal function is called by \code{link{detect.model.type}} and
+#'		\code{\link{predict.glmmML}} functions.
+#-------------------------------------------------------------------------------
+#	familyのフォーマットを揃える。
+#
+#	Args:
+#		family: familyオブジェクト、関数、文字列、symbol
+#		type:
+#			familyならfamilyオブジェクトを、characterならfamily名を表す文字列
+#			を返す。
+#-------------------------------------------------------------------------------
+format.family <- function(family, type = c("family", "character")) {
+	type <- match.arg(type)
+	if (is.character(family)) {
+		family <- get(family)
+	}
+	if (is.symbol(family)) {
+		family <- eval(family)
+	}
+	if (is.function(family)) {
+		family <- family()
+	}
+	if (is.null(family$family)) {
+		print(family)
+		stop("`family' not recognized")
+	}
+	result <- switch(type, family = family, character = family$family)
+	return(result)
+}
+
+
