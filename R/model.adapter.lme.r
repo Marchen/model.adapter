b004da6dfadafee7c6d07ccd4da1ffb6e4053df8
Michio Oguro <michio_oguro@hotmail.com>
2016-05-05 03:12:43 +0900

    Added get.call() method for lme. lme用のget.call()メソッドを追加。

--- R/model.adapter.lme.r
+++ R/model.adapter.lme.r
@@ -10,3 +10,17 @@
 model.adapter.lme <- setRefClass(
 	"model.adapter.lme", contains = "model.adapter"
 )
+
+
+#-------------------------------------------------------------------------------
+#	モデルオブジェクトからcallを取得する。
+#-------------------------------------------------------------------------------
+model.adapter.lme$methods(
+	get.call = function(x) {
+		call.list <- as.list(x$call)
+		call.list[[1]] <- substitute(lme)
+		return(as.call(call.list))
+	}
+)
+
+
