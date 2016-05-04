e3a090daa1887b9d36a7bc01082e355288f20346
Michio Oguro <michio_oguro@hotmail.com>
2016-05-05 03:58:12 +0900

    Moved is.formula() function to utils.r is.formula()関数をutils.rに移動。

--- R/utils.r
+++ R/utils.r
@@ -82,3 +82,20 @@ match.generic.call <- function(call, envir = parent.frame(2L)) {
 }
 
 
+#-------------------------------------------------------------------------------
+#'	(Internal) Check an object is formula.
+#'
+#'	@param x an object.
+#'	@return returns TRUE if \emph{x} is formula otherwise returns FALSE.
+#-------------------------------------------------------------------------------
+#	変数がformulaかを調べる。
+#
+#	Args:
+#		x: 変数。
+#	Value:
+#		xがformulaならTRUE、違えばFALSE。
+#-------------------------------------------------------------------------------
+is.formula <- function(x) {
+	return(is(x, "formula"))
+}
+
