7b2e564148b8de3e6c9a2e19f253d08577cf8e3b
Michio Oguro <michio_oguro@hotmail.com>
2016-05-05 03:35:16 +0900

    Added match.generic.call() function which match arguments of the call with actual function rather than a definition of generic function. 総称関数ではなく実際に呼び出される関数に対してmatch.call()を呼び出すmatch.generic.call()関数の追加。

--- R/utils.r
+++ R/utils.r
@@ -31,3 +31,54 @@ is.generic <- function(fun.name) {
 }
 
 
+#-------------------------------------------------------------------------------
+#'	(Internal) match.call() with handling of generic function.
+#'
+#'	Some generic functions have different arguments for different classes. In 
+#'	such cases, match.call() function returns wrong results. This function 
+#'	finds actual generic function called for focal class and matches call with
+#'	the function. If call is not a call for non-generic function, matching is
+#'	done by match.call() function.
+#'
+#'	@param call a call to be matched.
+#'	@param envir an environment to evaluate call.
+#'
+#'	@return matched call.
+#'
+#'	@examples
+#'		match.generic.call(substitute(hist(1:10)))
+#-------------------------------------------------------------------------------
+#	総称関数もたどってmatch.call()を行う。
+#
+#	Args:
+#		call: 引数をフルネームにするcall。
+#		envir: callを評価する環境。
+#
+#	Value:
+#		引数をフルネームにしたcall。
+#-------------------------------------------------------------------------------
+match.generic.call <- function(call, envir = parent.frame(2L)) {
+	fun.name <- get.function(call)
+	fun <- match.fun(fun.name)
+	matched.call <- match.call(fun, call)
+	if (!is.generic(fun.name)) {
+		# Non-generic functions.
+	   	return(matched.call)
+	}
+	# Find class of generic function / 総称関数の分岐に使われるクラスを取得。
+	generic.class.name <- names(formals(fun))[1]
+	generic.class <- class(eval(matched.call[[generic.class.name]], envir))
+	if (isGeneric(fun.name)) {
+		#S4 functions.
+		stop("Not implimented.")
+	} else {
+	   	#S3 functions.
+	   	fun <- getS3method(fun.name, generic.class, TRUE)
+	   	if (is.null(fun)) {
+	   		fun <- getS3method(fun.name, "default")
+	   	}
+		return(match.call(fun, call))
+	}
+}
+
+
