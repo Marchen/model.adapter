059e2123affc9e5d6c1dc24e84b4437930276d64
Michio Oguro <michio_oguro@hotmail.com>
2016-05-14 04:03:27 +0900

    Improved performance of tests of cforest. cforestのテストのパフォーマンスを向上。

--- tests/test_function__cforest.r
+++ tests/test_function__cforest.r
@@ -1,6 +1,11 @@
 # Test for cforest
 test__all(
-	call = substitute(cforest(Sepal.Length ~ ., data = iris)),
+	call = substitute(
+		cforest(
+			Sepal.Length ~ ., data = iris, 
+			controls = cforest_control(ntree = 10)
+		)
+	),
 	function.name = "cforest",
 	formula = Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
 	object.has.call = FALSE,
