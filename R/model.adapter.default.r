7b52edc570d1d456d924d1f62338eb7422c7504c
Michio Oguro <michio_oguro@hotmail.com>
2016-06-03 11:20:46 +0900

    Restructured definitions to the code works correctly. コードが正しく動くように定義を並べ替えた。

--- R/model.adapter.default.r
+++ R/model.adapter.default.r
@@ -1,3 +1,182 @@
+
+
+model.interface <- function(x) {
+	UseMethod("model.interface")
+}
+
+
+model.interface.default <- setRefClass(
+	"model.interface"
+)
+
+
+#-------------------------------------------------------------------------------
+#	モデルのfamilyを取得する。
+#	Args:
+#		x: 関数呼び出しのcall、もしくはモデルオブジェクト。
+#-------------------------------------------------------------------------------
+model.interface.default$methods(
+	get.family = function(x) {
+		"
+		Get family from call or model object. 
+		If family was not specified, return NULL.
+		\\describe{
+			\\item{\\code{x}}{call or model object.}
+		}
+		"
+		if (is.call(x)) {
+			return(x$family)
+		} else {
+			if (isS4(x)) {
+				if ("family" %in% slotNames(x)) {
+					return(src$object@family)
+				} else {
+					return(NULL)
+				}
+			} else {
+				return(x$family)
+			}
+		}
+	}
+)
+
+
+#-------------------------------------------------------------------------------
+#	モデルのcallを取得する。
+#	Args:
+#		x: はモデルオブジェクト。
+#	Value:
+#		オブジェクトを作ったcall。
+#-------------------------------------------------------------------------------
+model.interface.default$methods(
+	get.call = function(x) {
+		"
+		This method returns call by which the object is made. If call is not 
+		available, this returns NULL. To distinguish the return value of NULL 
+		is intended action or not, inherited classes are encouraged to 
+		inherit this method to explicitly return NULL if x does not have call.
+		\\describe{\\item{\\code{x}}{a model object.}}
+		"
+		if (isS4(x)) {
+			result <- x@call
+		} else {
+			result <- x$call
+			if (is.null(result)) {
+				warning("get.call() implicitly returns NULL. Is it intended?")
+			}
+		}
+		return(result)
+	}
+)
+
+
+
+#-------------------------------------------------------------------------------
+#	モデルのdataを取得する。
+#	Args:
+#		x: 関数呼び出しのcall、もしくはモデルオブジェクト。
+#		envir: xに入ったcallを評価する環境。
+#-------------------------------------------------------------------------------
+model.interface.default$methods(
+	get.data = function(x, envir = parent.frame()) {
+		"
+		Get a data.frame containing the data used for modeling.
+		If data is not available this method returns empty data.frame made by
+		data.frame().
+		\\describe{
+			\\item{\\code{x}}{
+				a model object/call from which data is extracted.
+			}
+			\\item{\\code{envir = parent.frame()}}{
+				an environment in which call is evaluated.
+			}
+		}
+		"
+		if (is.call(x)) {
+			d <- eval(x$data, envir)
+		} else {
+			if (isS4(x)) {
+				d <- x@data
+			} else {
+				d <- x$data
+			}
+			if (is.null(d)) {
+				# When couldn't retrieve data from object, get it from call.
+				# オブジェクトからdataを取得できなかったらcallから取得を試みる。
+				cl <- match.generic.call(.self$get.call(x))
+				d <- eval(cl$data, envir)
+			}
+		}
+		return(d)
+	}
+)
+
+
+#-------------------------------------------------------------------------------
+#	モデルのformulaを取得する。
+#	Args:
+#		x: 関数呼び出しのcall、もしくはモデルオブジェクト。
+#		envir: xに入ったcallを評価する環境。
+#-------------------------------------------------------------------------------
+model.interface.default$methods(
+	get.formula = function(x, envir = parent.frame()) {
+		"
+		Extract formula from model object/call.
+		\\describe{
+			\\item{\\code{x}}{
+				a model object/call from which formula is extracted.
+			}
+			\\item{\\code{envir = parent.frame()}}{
+				an environment in which call in x is evaluated.
+			}
+		}
+		"
+		if (is.object(x)) {
+			if (isS4(x)) {
+				return(eval(x@call$formula, envir))
+			} else {
+				return(eval(x$call$formula, envir))
+			}
+		} else {
+			if (!is.null(x$formula)) {
+				return(eval(x$formula, envir))
+			} else {
+				args <- lapply(as.list(x), eval, envir = envir)
+				f <- args[sapply(args, is.formula)][[1]]
+				return(f)
+			}
+		}
+	}
+)
+
+
+#-------------------------------------------------------------------------------
+#	formulaの.を展開する。
+#-------------------------------------------------------------------------------
+model.interface.default$methods(
+	expand.formula = function(f, d, specials = NULL, package.name = NULL) {
+		"
+		Expand . in formula.
+		\\describe{
+			\\item{\\code{f}}{a formula to expand.}
+			\\item{\\code{d}}{a data.frame used to expand . in formula.}
+			\\item{\\code{specials = NULL}}{
+				special characterss passed to 
+				\\code{\\link[stats]{terms.formula}}.
+			}
+			\\item{\\code{package.name = NULL}}{
+				a character literal of package name having the model function.
+			}
+		}
+		"
+		result <- terms(f, data = d, specials = specials)
+		attributes(result) <- NULL
+		result <- as.formula(result)
+		return(result)
+	}
+)
+
+
 #-------------------------------------------------------------------------------
 #	モデルの違いを吸収するアダプタークラスのオブジェクトを作る関数。
 #
@@ -210,74 +389,12 @@ model.adapter$methods(
 )
 
 
-model.interface <- function(x) {
-	UseMethod("model.interface")
-}
 
+#for (field.name in names(model.adapter.default$fields)) {
+	#model.adapter.default$lock(n)
+#}
+#rm(field.name)
 
-model.interface.default <- setRefClass(
-	"model.interface"
-)
-
-
-#-------------------------------------------------------------------------------
-#	モデルのfamilyを取得する。
-#	Args:
-#		x: 関数呼び出しのcall、もしくはモデルオブジェクト。
-#-------------------------------------------------------------------------------
-model.interface.default$methods(
-	get.family = function(x) {
-		"
-		Get family from call or model object. 
-		If family was not specified, return NULL.
-		\\describe{
-			\\item{\\code{x}}{call or model object.}
-		}
-		"
-		if (is.call(x)) {
-			return(x$family)
-		} else {
-			if (isS4(x)) {
-				if ("family" %in% slotNames(x)) {
-					return(src$object@family)
-				} else {
-					return(NULL)
-				}
-			} else {
-				return(x$family)
-			}
-		}
-	}
-)
-
-
-#-------------------------------------------------------------------------------
-#	モデルのcallを取得する。
-#	Args:
-#		x: はモデルオブジェクト。
-#	Value:
-#		オブジェクトを作ったcall。
-#-------------------------------------------------------------------------------
-model.interface.default$methods(
-	get.call = function(x) {
-		"
-		This method returns call by which the object is made. If call is not 
-		available, this returns NULL. To distinguish the return value of NULL 
-		is intended action or not, inherited classes are encouraged to 
-		inherit this method to explicitly return NULL if x does not have call.
-		\\describe{\\item{\\code{x}}{a model object.}}
-		"
-		if (isS4(x)) {
-			result <- x@call
-		} else {
-			result <- x$call
-			if (is.null(result)) {
-				warning("get.call() implicitly returns NULL. Is it intended?")
-			}
-		}
-		return(result)
-	}
-)
 
 
 #-------------------------------------------------------------------------------
@@ -298,47 +415,6 @@ model.adapter$methods(
 
 
 #-------------------------------------------------------------------------------
-#	モデルのdataを取得する。
-#	Args:
-#		x: 関数呼び出しのcall、もしくはモデルオブジェクト。
-#		envir: xに入ったcallを評価する環境。
-#-------------------------------------------------------------------------------
-model.interface.default$methods(
-	get.data = function(x, envir = parent.frame()) {
-		"
-		Get a data.frame containing the data used for modeling.
-		If data is not available this method returns empty data.frame made by
-		data.frame().
-		\\describe{
-			\\item{\\code{x}}{
-				a model object/call from which data is extracted.
-			}
-			\\item{\\code{envir = parent.frame()}}{
-				an environment in which call is evaluated.
-			}
-		}
-		"
-		if (is.call(x)) {
-			d <- eval(x$data, envir)
-		} else {
-			if (isS4(x)) {
-				d <- x@data
-			} else {
-				d <- x$data
-			}
-			if (is.null(d)) {
-				# When couldn't retrieve data from object, get it from call.
-				# オブジェクトからdataを取得できなかったらcallから取得を試みる。
-				cl <- match.generic.call(.self$get.call(x))
-				d <- eval(cl$data, envir)
-			}
-		}
-		return(d)
-	}
-)
-
-
-#-------------------------------------------------------------------------------
 #	dataフィールドにデータがあるかを確認。
 #-------------------------------------------------------------------------------
 model.adapter$methods(
@@ -351,74 +427,3 @@ model.adapter$methods(
 		return(result)
 	}
 )
-
-
-#-------------------------------------------------------------------------------
-#	モデルのformulaを取得する。
-#	Args:
-#		x: 関数呼び出しのcall、もしくはモデルオブジェクト。
-#		envir: xに入ったcallを評価する環境。
-#-------------------------------------------------------------------------------
-model.interface.default$methods(
-	get.formula = function(x, envir = parent.frame()) {
-		"
-		Extract formula from model object/call.
-		\\describe{
-			\\item{\\code{x}}{
-				a model object/call from which formula is extracted.
-			}
-			\\item{\\code{envir = parent.frame()}}{
-				an environment in which call in x is evaluated.
-			}
-		}
-		"
-		if (is.object(x)) {
-			if (isS4(x)) {
-				return(eval(x@call$formula, envir))
-			} else {
-				return(eval(x$call$formula, envir))
-			}
-		} else {
-			if (!is.null(x$formula)) {
-				return(eval(x$formula, envir))
-			} else {
-				args <- lapply(as.list(x), eval, envir = envir)
-				f <- args[sapply(args, is.formula)][[1]]
-				return(f)
-			}
-		}
-	}
-)
-
-
-#-------------------------------------------------------------------------------
-#	formulaの.を展開する。
-#-------------------------------------------------------------------------------
-model.interface.default$methods(
-	expand.formula = function(f, d, specials = NULL, package.name = NULL) {
-		"
-		Expand . in formula.
-		\\describe{
-			\\item{\\code{f}}{a formula to expand.}
-			\\item{\\code{d}}{a data.frame used to expand . in formula.}
-			\\item{\\code{specials = NULL}}{
-				special characterss passed to 
-				\\code{\\link[stats]{terms.formula}}.
-			}
-			\\item{\\code{package.name = NULL}}{
-				a character literal of package name having the model function.
-			}
-		}
-		"
-		result <- terms(f, data = d, specials = specials)
-		attributes(result) <- NULL
-		result <- as.formula(result)
-		return(result)
-	}
-)
-
-
-for (field.name in names(model.adapter.default$fields)) {
-	model.adapter.default$lock(n)
-}
-rm(field.name)
