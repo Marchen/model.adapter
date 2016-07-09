1524c5a56cb7f738f57a928e7ef5297760fa4ae3
Michio Oguro <michio_oguro@hotmail.com>
2016-07-09 16:12:30 +0900

    Updated test for glm and MCMCglmm. glmとMCMCglmmのテストを更新した。

--- tests/test_function__MCMCglmm.r
+++ tests/test_function__MCMCglmm.r
@@ -1,4 +1,67 @@
 # Test for MCMCglmm
+iris2 <- iris
+iris2$bin <- ifelse(iris2$Species == "setosa", 1, 0)
+iris2$bin1 <- ifelse(iris2$Species == "setosa", 1, 0)
+iris2$bin2 <- ifelse(iris2$Species == "setosa", 0, 1)
+
+link.test = ma.link.test(
+	list(
+		substitute(
+			MCMCglmm(
+				Sepal.Length ~ Petal.Length, data = iris2, 
+				family = "gaussian", verbose = FALSE
+			)
+		),
+		substitute(
+			MCMCglmm(
+				bin ~ Petal.Length, data = iris2, family = "poisson", 
+				verbose = FALSE
+			)
+		),
+		substitute(
+			MCMCglmm(
+				bin ~ Petal.Length, data = iris2, family = "categorical",
+				verbose = FALSE
+			)
+		),
+		substitute(
+			MCMCglmm(
+				cbind(bin1, bin2) ~ Petal.Length, data = iris2,
+				family = "multinomial2", verbose = FALSE
+			)
+		),
+		substitute(
+			MCMCglmm(
+				bin ~ Petal.Length, data = iris2, family = "geometric",
+				verbose = FALSE
+			)
+		),
+		substitute(
+			MCMCglmm(
+				bin ~ Petal.Length, data = iris2, family = "exponential",
+				verbose = FALSE
+			)
+		)
+	),
+	list(
+		identity,
+		log,
+		binomial()$linkfun,
+		binomial()$linkfun,
+		binomial()$linkfun,
+		function(x) -log(x)
+	),
+	list(
+		identity,
+		exp,
+		binomial()$linkinv,
+		binomial()$linkinv,
+		binomial()$linkinv,
+		function(x) exp(-x)
+	)
+)
+
+
 test__all(
 	call = substitute(
 		MCMCglmm(
@@ -11,5 +74,11 @@ test__all(
 	object.has.call = FALSE,
 	object.has.data = FALSE,
 	family = "gaussian",
-	data = iris
+	data = iris,
+	link.test = link.test
 )
+
+
+rm(link.test, iris2)
+
+
--- tests/test_function__glm.r
+++ tests/test_function__glm.r
@@ -1,9 +1,56 @@
 # Test for glm
+iris2 <- iris
+iris2$bin <- ifelse(iris2$Species == "setosa", 1, 0)
+link.test = ma.link.test(
+	list(
+		substitute(glm(Sepal.Length ~ ., data = iris2, family = gaussian)),
+		substitute(glm(Sepal.Length ~ ., data = iris2, family = "gaussian")),
+		substitute(glm(Sepal.Length ~ ., data = iris2, family = gaussian("log"))),
+		substitute(glm(Sepal.Length ~ ., data = iris2, family = gaussian(log))),
+		substitute(glm(bin ~ ., data = iris2, family = poisson)),
+		substitute(glm(bin ~ ., data = iris2, family = binomial)),
+		substitute(glm(Sepal.Length ~ ., data = iris2, family = Gamma)),
+		substitute(glm(Sepal.Length ~ ., data = iris2, family = inverse.gaussian)),
+		substitute(glm(Sepal.Length ~ ., data = iris2, family = quasi)),
+		substitute(glm(bin ~ ., data = iris2, family = quasibinomial)),
+		substitute(glm(bin ~ ., data = iris2, family = quasipoisson))
+	),
+	list(
+		gaussian()$linkfun,
+		gaussian()$linkfun,
+		gaussian(log)$linkfun,
+		gaussian(log)$linkfun,
+		poisson()$linkfun,
+		binomial()$linkfun,
+		Gamma()$linkfun,
+		inverse.gaussian()$linkfun,
+		quasi()$linkfun,
+		quasibinomial()$linkfun,
+		quasipoisson()$linkfun
+	),
+	list(
+		gaussian()$linkinv,
+		gaussian()$linkinv,
+		gaussian(log)$linkinv,
+		gaussian(log)$linkinv,
+		poisson()$linkinv,
+		binomial()$linkinv,
+		Gamma()$linkinv,
+		inverse.gaussian()$linkinv,
+		quasi()$linkinv,
+		quasibinomial()$linkinv,
+		quasipoisson()$linkinv
+	)
+)
+
 test__all(
 	call = substitute(glm(Sepal.Length ~ ., data = iris, family = gaussian)),
 	function.name = "glm",
 	formula = Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
 	family = "gaussian",
-	data = iris
+	data = iris, link.test = link.test
 )
+
+rm(link.test, iris2)
+
 
