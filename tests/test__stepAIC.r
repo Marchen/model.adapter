4c872a8c5283ea4700f6dcfdf77db45993fd9545
Michio Oguro <michio_oguro@hotmail.com>
2017-01-06 15:30:02 +0900

    model.adapter() can work with the results of MASS::stepAIC. MASS::stepAICの結果にmodel.adapter()を対応させた。

---
+++ tests/test__stepAIC.r
@@ -0,0 +1,32 @@
+library(nlme)
+library(testthat)
+
+# Prepare objects and calls for testing.
+calls <- list(
+	glm = substitute(
+		glm(
+			Sepal.Length ~ Species + Petal.Length,
+			data = iris, family = gaussian
+		)
+	),
+	lm = substitute(lm(Sepal.Length ~ Species + Petal.Length, data = iris)),
+	lme = substitute(
+		lme(
+			Sepal.Length ~ Species + Petal.Length, random = ~1 | Species,
+			data = iris, method = "ML"
+		)
+	)
+)
+
+objects <- lapply(calls, eval)
+
+# do MASS::stepAIC.
+result.stepAIC <- lapply(objects, MASS::stepAIC)
+
+# Test result of stepAIC does not produce errors.
+expect_silent(
+	null <- lapply(result.stepAIC, model.adapter)
+)
+
+# Remove objects.
+rm(calls, objects, result.stepAIC, null)
