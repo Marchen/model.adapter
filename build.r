260a7f11a833147b327476ca11d87995d6aa2764
Michio Oguro <michio_oguro@hotmail.com>
2017-01-06 16:30:41 +0900

    Updated build script. ビルドスクリプトの更新。

--- build.r
+++ build.r
@@ -3,33 +3,58 @@ require(devtools)
 
 
 get.this.file.dir <- function() {
-   	cmdArgs <- commandArgs(trailingOnly = FALSE)
-   	needle <- "--file="
-   	match <- grep(needle, cmdArgs)
-   	if (length(match) > 0) {
-   		# Rscript
-   		return(dirname(sub(needle, "", cmdArgs[match])))
-   	} else {
-   		# 'source'd via R console
-   		return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
-   	}
+	cmdArgs <- commandArgs(trailingOnly = FALSE)
+	needle <- "--file="
+	match <- grep(needle, cmdArgs)
+	if (length(match) > 0) {
+		# Rscript
+		return(dirname(sub(needle, "", cmdArgs[match])))
+	} else {
+		# 'source'd via R console
+		return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
+	}
 }
 
 setwd(get.this.file.dir())
 
-roxygenize(clean = TRUE)
-build(path = "../repos/src/contrib")
-if (version$os == "mingw32") {
-	build(
-		binary = TRUE, args = "--preclean",
-		path = "../repos/bin/windows/contrib/3.3/"
-	)
-} else {
-	build(
-		binary = TRUE, args = "--preclean",
-		path = "../repos/bin/macosx/mavericks/contrib/3.3/"
-	)
-}
-install()
 
+#-------------------------------------------------------------------------------
+#	Build package
+#-------------------------------------------------------------------------------
+# Convert documents.
+roxygenize(clean = TRUE)
+
+# Build source package
+build(path = "../repos/src/contrib")
+
+# Build binary package
+if (version$os == "mingw32") {
+	bin.path <- "../repos/bin/windows/contrib/%s/"
+} else {
+	bin.path = "../repos/bin/macosx/mavericks/contrib/%s/"
+}
+r.ver <- paste(version$major, strsplit(version$minor, "\\.")[[1]][1], sep = ".")
+bin.path <- sprintf(bin.path, r.ver)
+if (!dir.exists(bin.path)) {
+	dir.create(bin.path)
+}
+build(binary = TRUE, args = "--preclean", path = bin.path)
+
+
+#-------------------------------------------------------------------------------
+#	Deploy
+#-------------------------------------------------------------------------------
+path.repos <- file.path(get.this.file.dir(), "../repos/")
+
+tools::write_PACKAGES(
+	file.path(path.repos, "src/contrib"), type = "source"
+)
+tools::write_PACKAGES(
+	file.path(path.repos, sprintf("bin/windows/contrib/%s/", r.ver)),
+	type = "win.binary"
+)
+tools::write_PACKAGES(
+	file.path(path.repos, sprintf("bin/windows/contrib/%s/", r.ver)),
+	type = "mac.binary"
+)
 
