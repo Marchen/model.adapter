e288f0141eacda80333617ff998a43e86cabb357
Michio Oguro <michio_oguro@hotmail.com>
2017-02-19 08:53:42 +0900

    Add SUPPORTED_MODELS constant whichi shows supported models. サポートされているモデルを示すSUPPORTED_MODELS定数の追加。

---
+++ R/const.r
@@ -0,0 +1,5 @@
+SUPPORTED_MODELS <- c(
+	"cforest", "ctree", "gam::gam", "mgcv::gam", "mgcv::gamm", "gbm", "glm",
+	"glmer", "glmmadmb", "glmmML", "lm",  "lme", "lmer", "MCMCglmm",
+	"randomForest", "ranger", "rpart", "svm", "tree"
+)
