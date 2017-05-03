972eab09246b7951ab9b1348dbc0b1281acca504
Michio Oguro <michio_oguro@hotmail.com>
2017-05-03 11:28:00 +0900

    Add predict method to model.interface.glmmML. model.interface.glmmMLにpredictメソッドを追加した。

--- R/model.interface.glmmML.r
+++ R/model.interface.glmmML.r
@@ -42,3 +42,68 @@ model.interface.glmmML$methods(
 		return(make.predict.types(prob = "response", class = "response"))
 	}
 )
+
+
+#-------------------------------------------------------------------------------
+#	glmmML用のpredictメソッド。
+#
+#	Args:
+#		object:
+#			glmmMLオブジェクト。
+#		newdata:
+#			予測に使うデータが入ったデータフレーム。
+#			指定しなかった場合、予測に用いたデータを使うがglmmMLオブジェクトには
+#			データが保存されていないので、データがワークスペースに残っていない場合、
+#			データの取得に失敗する可能性がある。
+#		type:
+#			予測を計算するスケール。
+#			デフォルトでは応答変数のスケールで予測を計算
+#			する。"link"を指定すると、リンク関数のスケールで予測を計算する。
+#		conditional:
+#			TRUEだとconditional（ランダム効果による切片の違いも考慮した）な
+#			予測値を計算する。
+#			FALSEだとmarginal（ランダム効果を無視した固定効果だけ）な予測値を
+#			計算する。
+#		...: 他のメソッドに渡される引数。
+#-------------------------------------------------------------------------------
+#'	predict method for glmmML object.
+#'
+#'	This is a predict method for \emph{glmmML} object.
+#'
+#'	@param object a glmmML object.
+#'	@param newdata
+#		a data.frame containing data used for prediction. If missing, the data
+#		used for the modeling is used. However, because glmmML object does not
+#		contain original data used for the modeling, retrieving the data used
+#		for the modeling may fail if the data is removed from workspace.
+#'	@param type
+#'		type of prediction. Default is scale of response variable.
+#'		If "link" is specified, prediction is of link scale is calculated.
+#'	@param conditional
+#		if FALSE, marginal (using fixed terms only and unconditioned to random
+#		effect) predicted values are calculated. If TRUE, predicted values
+#		conditioned to the random effect is calculated.
+#'	@param ... further arguments passed to other methods.
+#-------------------------------------------------------------------------------
+predict.glmmML <- function(
+	object, newdata, type = c("response", "link"), conditional = FALSE, ...
+) {
+	if (missing(newdata)) {
+		newdata <- eval(object$call$data)
+	}
+	type = match.arg(type)
+	design.matrix <- model.matrix(object, data = newdata)
+	result <- design.matrix %*% coef(object)
+	# ランダム効果を使う時にはランダム効果分の切片のずれを加算する。
+	if (conditional) {
+		cluster <- eval(object$call$cluster, newdata)
+		result <- result + object$posterior.mode[as.numeric(cluster)]
+	}
+	if (type == "link") {
+		return(result)
+	} else {
+		family()
+		family <- format.family(object$call$family, type = "family")
+		return(family$linkinv(result))
+	}
+}
