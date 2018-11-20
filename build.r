6eeb8a3d0942d396e7cb315fb491165569825a2f
Michio Oguro <michio_oguro@hotmail.com>
2018-11-20 15:29:17 +0900

    chore: Update build script. ビルドスクリプトの更新。

--- build.r
+++ build.r
@@ -22,62 +22,29 @@ old.wd <- setwd(get.this.file.dir())
 
 
 #------------------------------------------------------------------------------
-#	Get R version.
+#	Clean up previous builds
 #------------------------------------------------------------------------------
-r.ver <- paste(
-	version$major, strsplit(version$minor, "\\.")[[1]][1], sep = "."
-)
+unlink("build/*", recursive = TRUE)
 
 
 #------------------------------------------------------------------------------
-#	Install the package before compiling.
+#	Build documentation.
 #------------------------------------------------------------------------------
-system("Rscript -e library(devtools);install()")
-
-
-#------------------------------------------------------------------------------
-#	Convert documents.
-#------------------------------------------------------------------------------
+unlink("man", recursive = TRUE)
 document()
 
 
 #------------------------------------------------------------------------------
-#	Build package.
+#	Build source package
 #------------------------------------------------------------------------------
-# Build source package.
-repo.path = "../repos/src/contrib"
-if (!file.exists(repo.path)) {
-	dir.create(repo.path, recursive = TRUE)
+if (!file.exists("build")) {
+	dir.create("build")
 }
-build(path = repo.path)
-
-# Build windows binary package.
-if (version$os == "mingw32") {
-	bin.path <- "../repos/bin/windows/contrib/%s"
-	bin.path <- normalizePath(sprintf(bin.path, r.ver))
-	if (!file.exists(bin.path)) {
-		dir.create(bin.path)
-	}
-	build(binary = TRUE, args = "--preclean", path = bin.path)
-}
+build(path = "build", vignettes = FALSE)
 
 
 #------------------------------------------------------------------------------
-#	Deploy.
-#------------------------------------------------------------------------------
-path.repos <- file.path(get.this.file.dir(), "../repos/")
-
-tools::write_PACKAGES(
-	file.path(path.repos, "src/contrib"), type = "source"
-)
-tools::write_PACKAGES(
-	file.path(path.repos, sprintf("bin/windows/contrib/%s/", r.ver)),
-	type = "win.binary"
-)
-
-
-#------------------------------------------------------------------------------
-#	Cleanup.
+#	Restore working directory.
 #------------------------------------------------------------------------------
 setwd(old.wd)
 rm(old.wd)
