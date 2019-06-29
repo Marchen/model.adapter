8e1bbe595cb6f585b7ee240b2529ba37bcf0200a
Michio Oguro <michio_oguro@hotmail.com>
2019-06-30 01:13:34 +0900

    test: Update the test for ranger. rangerのテストを更新。

--- tests/test__model.adapter__ranger.r
+++ tests/test__model.adapter__ranger.r
@@ -3,19 +3,28 @@
 #==============================================================================
 
 source("tests.r")
+iris2 <- iris
+iris2$Species <- as.character(iris2$Species)
 
 test.data <- list(
 	call = list(
 		substitute(
 			ranger(Sepal.Length ~ ., data = iris, write.forest = TRUE)
 		),
-		substitute(ranger(Species ~ ., data = iris, write.forest = TRUE))
+		substitute(ranger(Species ~ ., data = iris, write.forest = TRUE)),
+		substitute(
+			ranger(
+				Species ~ ., data = iris2, write.forest = TRUE,
+				probability = TRUE
+			)
+		)
 	),
 	formula = list(
 		Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species,
+		Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width,
 		Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
 	),
-	model.type = list("regression", "classification")
+	model.type = list("regression", "classification", "classification")
 )
 
 test.model.adapter("ranger", iris, test.data)
