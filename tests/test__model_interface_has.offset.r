b83dc1c9f552ee9ed689d072f9dffbe2e2b43248
Michio Oguro <michio_oguro@hotmail.com>
2019-06-29 22:40:39 +0900

    test: Add tests for model.interface.default$has.offset.argument() and model.interface.default$has.offset.in.formula(). model.interface.default$has.offset.argument()とmodel.interface.default$has.offset.argument()のテストを追加。

---
+++ tests/test__model_interface_has.offset.r
@@ -0,0 +1,60 @@
+#==============================================================================
+#	Test for has.offset method in model.interface.
+#==============================================================================
+
+#------------------------------------------------------------------------------
+#	Create test data.
+#------------------------------------------------------------------------------
+test.data <- list(
+	call.no.offset = list(
+		x = substitute(glm(Petal.Length ~ ., data = iris)),
+		has.offset.argument = FALSE, has.offset.in.formula = FALSE
+	),
+	call.formula = list(
+		x = substitute(
+			glm(Petal.Length ~ . + offset(Sepal.Width), data = iris)
+		),
+		has.offset.argument = FALSE, has.offset.in.formula = TRUE
+	),
+	call.argument = list(
+		x = substitute(
+			glm(Petal.Length ~ ., offset = Sepal.Width, data = iris)
+		),
+		has.offset.argument = TRUE, has.offset.in.formula = FALSE
+	),
+	call.both = list(
+		x = substitute(
+			glm(
+				Petal.Length ~ . + offset(Petal.Width),
+				offset = Sepal.Width, data = iris
+			)
+		),
+		has.offset.argument = TRUE, has.offset.in.formula = TRUE
+	)
+)
+
+for (i in names(test.data)) {
+	test.data[[gsub("^call", "object", i)]] <- test.data[[i]]
+	test.data[[i]]$x <- eval(test.data[[i]]$x)
+}
+
+
+#------------------------------------------------------------------------------
+#	Run tests.
+#------------------------------------------------------------------------------
+test_that(
+	"Testing model.interface.derault$has.offset()",
+	{
+		interface <- model.adapter:::model.interface.default()
+		for (i in names(test.data)) {
+			expect_equal(
+				interface$has.offset.in.formula(
+					test.data[[i]]$x, .GlobalEnv, "stats"
+				),
+				test.data[[i]]$has.offset.in.formula,
+				info = i
+			)
+		}
+	}
+)
+
